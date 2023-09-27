a9e6b11c3e2d1478d242f1a1430a95e18d2d825b
suhani <csb.3799@gmail.com>
2023-09-27 10:00:50 +0530

    added

--- bank_segmentation.sql
+++ bank_segmentation.sql
@@ -12,3 +12,14 @@ WHERE t.transaction_type = 'debit'
 GROUP BY c.customer_id, c.name
 ORDER BY total_spent DESC;
 
+-- 2. Salary Trend Analysis
+SELECT 
+    TO_CHAR(DATE_TRUNC('month', t.transaction_date), 'YYYY-MM') AS month,
+    SUM(t.amount) AS total_salary_credited
+FROM transactions t
+WHERE t.transaction_type = 'credit'
+  AND LOWER(t.description) LIKE '%salary credited%'
+  AND t.transaction_date >= NOW() - INTERVAL '12 months'
+GROUP BY month
+ORDER BY month;
+
